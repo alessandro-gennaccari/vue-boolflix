fe6e55122e6caeeb0b85eff40ba6b6f6ad6191ce
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-02-10 19:05:22 +0100

    added.tv-series_vote-round

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ var boolflix = new Vue({
         query: '',
         api_key: 'bc3be1f92a6701a8589c3f5cc8995f46',
         lang: 'it-IT',
-        searcharray: []
+        searcharray: [],
+
     },
     methods: {
         search() {
@@ -23,9 +24,30 @@ var boolflix = new Vue({
             })
             .catch((error) => console.log(error));
 
+            // Chiamata axios per serie tv
+            axios
+            .get('https://api.themoviedb.org/3/search/tv', {
+                params: {
+                    api_key: this.api_key,
+                    query: this.query,
+                    language: this.lang
+                }
+            })
+            .then(result => {
+                this.searcharray = this.searcharray.concat(result.data.results);
+                this.voteStar(this.searcharray);
+            })
+            .catch((error) => console.log(error));
+
             // Svuota il campo di input
             this.query = '';
 
+        },
+        voteStar(array){
+            array.forEach(element => {
+                element.vote_average = Math.round(element.vote_average / 2);
+                console.log(element.vote_average);
+            });
         }
     }
 }); 
